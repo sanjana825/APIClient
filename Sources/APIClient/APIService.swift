b3425ee9218c6ec4b33d05ab78836eb07bd3cb14
sanjana825 <sanjanasb253@gmail.com>
2025-01-04 17:15:23 +0530

    Commiting changes

--- Sources/APIClient/APIService.swift
+++ Sources/APIClient/APIService.swift
@@ -1,8 +1,5 @@
 //
 //  APIService.swift
-//  SwiftBuy
-//
-//  Created by Sanjana on 26/12/24.
 //
 
 import Foundation
@@ -10,32 +7,36 @@ import Combine
 
 class APIService{
     
+    public init() { }
+    
+    @available(macOS 10.15, *)
+    @available(iOS 13.0, *)
     func sendRequests<T: Decodable>(url: String, method: HTTPMethod, decodingType: T.Type) -> AnyPublisher<T, NetworkError> {
         guard let url = URL(string: url) else {
-            return Fail(error: NetworkError.invalidURL)
-                .eraseToAnyPublisher()
+                return Fail(error: NetworkError.invalidURL)
+                    .eraseToAnyPublisher()
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
-        
-        return URLSession.shared.dataTaskPublisher(for: request)
-            .mapError { _ in NetworkError.requestFailed }
-            .map(\.data)
-            .decode(type: decodingType, decoder: JSONDecoder())
-            .mapError { _ in NetworkError.decodingFailed }
-            .eraseToAnyPublisher()
+    
+            return URLSession.shared.dataTaskPublisher(for: request)
+                .mapError { _ in NetworkError.requestFailed }
+                .map(\.data)
+                .decode(type: decodingType, decoder: JSONDecoder())
+                .mapError { _ in NetworkError.decodingFailed }
+                .eraseToAnyPublisher()
     }
 }
 
-enum NetworkError: Error {
+public enum NetworkError: Error {
     case invalidURL
     case requestFailed
     case decodingFailed
     case timeout
 }
 
-enum HTTPMethod: String {
+public enum HTTPMethod: String {
     case get = "GET"
     case post = "POST"
     case put = "PUT"
@@ -44,7 +45,7 @@ enum HTTPMethod: String {
 }
 
 extension NetworkError: LocalizedError {
-    var errorDescription: String?{
+    public var errorDescription: String?{
         switch self{
         case .invalidURL: return NSLocalizedString("Invalid URL", comment: "Invalid URL")
         case .requestFailed: return NSLocalizedString("Request Failed", comment: "Request Failed")
